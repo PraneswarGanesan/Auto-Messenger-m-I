6898a40e17676014ffc9f45e54d306e927a0ca8f
Praneswar G <118578571+PraneswarGanesan@users.noreply.github.com>
2023-10-31 12:44:42 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,41 @@
+import time
+import pyautogui
+
+# Delay before starting (adjust as needed)
+time.sleep(3)
+
+# Open WhatsApp and navigate to the chat window of the recipient
+# Ensure that the WhatsApp application is already open and visible on your screen
+
+# Move the mouse to the coordinates of the WhatsApp chat window
+# Replace the (x, y) values with the appropriate coordinates
+# These coordinates can be determined by using the pyautogui.position() function
+x =  100 # Replace with the x-coordinate of the chat window
+y = 200  # Replace with the y-coordinate of the chat window
+pyautogui.moveTo(x, y)
+
+# Read the contents of the text file and remove leading tab spaces
+file_path = r"C:\Users\Praneswar\.vscode\Python\Python programs\type.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    lines = file.readlines()
+
+# Remove leading tab spaces from each line
+contents = [line.lstrip() for line in lines]
+
+# Now 'contents' is a list of lines without leading tab spaces
+# You can join them back into a single string if needed
+contents = "".join(contents)
+
+# Set the typing speed (adjust as needed)
+typing_speed =  5 # Words per minute
+
+# Calculate the delay between each character
+delay = 0.65 # Delay in seconds
+
+# Type the contents of the file with the desired typing speed
+for char in contents:
+    pyautogui.typewrite(char)
+    time.sleep(delay)
+
+# Press Enter to send the message
+pyautogui.press("enter")
